584d3264acd7748e87b83c329197d900f0e4031d
Czarap <overriderlightborn11104@gmail.com>
2025-09-13 23:17:38 +0800

    Update

--- backend/routes/public_routes.py
+++ backend/routes/public_routes.py
@@ -8,3 +8,7 @@ public_routes = Blueprint("public_routes", __name__)
 @public_routes.route("/")
 def home():
     return render_template("index.html")
+
+@public_routes.route("/about")
+def about():
+    return render_template("about.html")
