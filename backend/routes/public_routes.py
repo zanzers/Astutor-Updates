4cce3af06c1127a548f816eedba2d8b565d2e81d
Czarap <overriderlightborn11104@gmail.com>
2025-09-21 20:41:40 +0800

    frontup

--- backend/routes/public_routes.py
+++ backend/routes/public_routes.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, request, send_from_directory
 
-
-
-
 public_routes = Blueprint("public_routes", __name__)
 
 @public_routes.route("/")
 def home():
     return render_template("index.html")
+
+@public_routes.route("/about")
+def about():
+    return render_template("about.html")
